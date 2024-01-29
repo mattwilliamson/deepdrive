633bae4d02f91fd325fc098f5e17a8bcf84a6b90
Matt Williamson <Matt@aimatt.com>
2024-01-29 10:03:33 -0500

    simulator working with skid steer, imu and joystick

--- src/deepdrive_description/README.md
+++ src/deepdrive_description/README.md
@@ -5,9 +5,26 @@ This has the URDF files and such to describe how to robot looks and behaves phys
 ```sh
 make dockersimshell
 ros2 launch deepdrive_description display.launch.py
-ros2 run teleop_twist_keyboard teleop_twist_keyboard
+ros2 run deepdrive_teleop teleop_keyboard use_sim_time:=true --ros-args --remap /cmd_vel:=/diff_drive_controller/cmd_vel_unstamped
 ```
 
 ```sh
 urdf-viz src/deepdrive_description/urdf/deepdrive_deepdrive.xacro
+```
+
+# Ros2 Control
+
+```sh
+
+ros2 control load_controller controller_manager
+ros2 control list_hardware_interfaces
+ros2 run deepdrive_teleop teleop_keyboard
+
+ros2 launch deepdrive_bringup launch_sim.launch.py world:=src/deepdrive_description/worlds/obstacles.world
+ros2 run deepdrive_teleop teleop_keyboard use_sim_time:=true --ros-args --remap /cmd_vel:=/diff_drive_controller/cmd_vel_unstamped
+
+ros2 run controller_manager spawner diff_drive_controller
+ros2 run controller_manager spawner joint_state_broadcaster
+
+
 ```
--- src/deepdrive_description/launch/display.launch.py
+++ src/deepdrive_description/launch/display.launch.py
@@ -1,57 +1,90 @@
 import launch
-from launch.substitutions import Command, LaunchConfiguration
-from launch.actions import IncludeLaunchDescription
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
+from launch.actions import (
+    IncludeLaunchDescription,
+    DeclareLaunchArgument,
+    RegisterEventHandler,
+)
 from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.substitutions import FindPackageShare
+from launch.event_handlers import OnProcessExit
+
 
 from ament_index_python import get_package_share_directory
 import launch_ros
+from launch_ros.actions import Node
 import os
 from launch.conditions import IfCondition
 
+
 def generate_launch_description():
-    pkg_share = launch_ros.substitutions.FindPackageShare(package='deepdrive_description').find('deepdrive_description')
-    default_model_path = os.path.join(pkg_share, 'urdf/deepdrive_deepdrive.xacro')
-    default_rviz_config_path = os.path.join(pkg_share, 'rviz/model.rviz')
-    
-    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
-    # world_path=os.path.join(pkg_share, 'worlds/deepdrive_world.world')
-    world_path = os.path.join(get_package_share_directory('deepdrive_gazebo'), 'worlds', 'deepdrive_house.world')
-    
-    robot_state_publisher_node = launch_ros.actions.Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
+    pkg_share = FindPackageShare(package="deepdrive_description").find(
+        "deepdrive_description"
     )
-    
-    joint_state_publisher_node = launch_ros.actions.Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        name='joint_state_publisher',
-        condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
+    default_model_path = os.path.join(pkg_share, "urdf/deepdrive_deepdrive.xacro")
+    default_rviz_config_path = os.path.join(pkg_share, "rviz/model.rviz")
+
+    pkg_gazebo_ros = get_package_share_directory("gazebo_ros")
+    world_path=os.path.join(pkg_share, 'worlds/deepdrive_world.world')
+    # world_path = os.path.join(
+    #     get_package_share_directory("deepdrive_gazebo"),
+    #     "worlds",
+    #     "deepdrive_house.world",
+    # )
+    robot_description = {
+        "robot_description": Command(["xacro ", LaunchConfiguration("model")])
+    }
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
     )
 
-    rviz_node = launch_ros.actions.Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        output='screen',
-        arguments=['-d', LaunchConfiguration('rvizconfig')],
+    joint_state_publisher_node = Node(
+        package="joint_state_publisher",
+        executable="joint_state_publisher",
+        name="joint_state_publisher",
+        condition=launch.conditions.UnlessCondition(LaunchConfiguration("gui")),
     )
 
-    spawn_entity = launch_ros.actions.Node(
-    	package='gazebo_ros', 
-    	executable='spawn_entity.py',
-        arguments=['-entity', 'deepdrive', '-topic', 'robot_description'],
-        output='screen'
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="screen",
+        arguments=["-d", LaunchConfiguration("rvizconfig")],
     )
 
-    robot_localization_node = launch_ros.actions.Node(
-         package='robot_localization',
-         executable='ekf_node',
-         name='ekf_filter_node',
-         output='screen',
-         parameters=[os.path.join(get_package_share_directory("deepdrive_bringup"), 'config', 'ekf.yaml'), {'use_sim_time': LaunchConfiguration('use_sim_time')}]
+    # rviz_node = Node(
+    #     package="rviz2",
+    #     executable="rviz2",
+    #     name="rviz2",
+    #     output="log",
+    #     arguments=["-d", rviz_config_file],
+    #     condition=IfCondition(gui),
+    # )
+
+    spawn_entity = Node(
+        package="gazebo_ros",
+        executable="spawn_entity.py",
+        arguments=["-entity", "deepdrive", "-topic", "robot_description"],
+        output="screen",
+    )
+
+    robot_localization_node = Node(
+        package="robot_localization",
+        executable="ekf_node",
+        name="ekf_filter_node",
+        output="screen",
+        parameters=[
+            os.path.join(
+                get_package_share_directory("deepdrive_bringup"), "config", "ekf.yaml"
+            ),
+            {"use_sim_time": LaunchConfiguration("use_sim_time")},
+        ],
     )
 
     foxglove_bridge = IncludeLaunchDescription(
@@ -64,23 +97,127 @@ def generate_launch_description():
         )
     )
 
+    robot_controllers = PathJoinSubstitution(
+        [
+            FindPackageShare("deepdrive_description"),
+            "config",
+            "controllers.yaml",
+        ]
+    )
 
-    return launch.LaunchDescription([
-        launch.actions.DeclareLaunchArgument(name='gui', default_value='False',
-                                            description='Flag to enable joint_state_publisher_gui'),
-        launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
-                                            description='Absolute path to robot urdf file'),
-        launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
-                                            description='Absolute path to rviz config file'),
-        launch.actions.DeclareLaunchArgument(name='use_sim_time', default_value='True',
-                                            description='Flag to enable use_sim_time'),
-        launch.actions.ExecuteProcess(cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so', world_path], output='screen'),
-        # gzserver_cmd,
-        # gzclient_cmd,
-        joint_state_publisher_node,
-        robot_state_publisher_node,
-        spawn_entity,
-        robot_localization_node,
-        rviz_node,
-        # foxglove_bridge
-    ])
+    control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        # parameters=[robot_description, robot_controllers],
+        parameters=[robot_controllers],
+        output="both",
+        # remappings=[(/robot_description)],
+    )
+    #  [controller_manager]: [Deprecated] Passing the robot description parameter directly to the control_manager node is deprecated. Use '~/robot_description' topic from 'robot_state_publisher' instead.
+    robot_state_pub_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
+        # parameters=[],
+        remappings=[
+            ("/diff_drive_controller/cmd_vel_unstamped", "/cmd_vel"),
+        ],
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "joint_state_broadcaster",
+            "--controller-manager",
+            "/controller_manager",
+        ],
+    )
+
+    robot_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "diff_drive_controller",
+            "--controller-manager",
+            "/controller_manager",
+        ],
+    )
+
+    # Delay rviz start after `joint_state_broadcaster`
+    delay_rviz_after_joint_state_broadcaster_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[rviz_node],
+        )
+    )
+
+    # Delay start of robot_controller after `joint_state_broadcaster`
+    delay_robot_controller_spawner_after_joint_state_broadcaster_spawner = (
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=joint_state_broadcaster_spawner,
+                on_exit=[robot_controller_spawner],
+            )
+        )
+    )
+
+    return launch.LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                name="gui",
+                default_value="False",
+                description="Flag to enable joint_state_publisher_gui",
+            ),
+            DeclareLaunchArgument(
+                name="model",
+                default_value=default_model_path,
+                description="Absolute path to robot urdf file",
+            ),
+            DeclareLaunchArgument(
+                name="rvizconfig",
+                default_value=default_rviz_config_path,
+                description="Absolute path to rviz config file",
+            ),
+            DeclareLaunchArgument(
+                name="use_sim_time",
+                default_value="True",
+                description="Flag to enable use_sim_time",
+            ),
+            launch.actions.ExecuteProcess(
+                cmd=[
+                    "gazebo",
+                    "--verbose",
+                    "-s",
+                    "libgazebo_ros_init.so",
+                    "-s",
+                    "libgazebo_ros_factory.so",
+                    world_path,
+                ],
+                output="screen",
+            ),
+            # gzserver_cmd,
+            # gzclient_cmd,
+            # joint_state_publisher_node,
+            spawn_entity,
+            # robot_localization_node,
+            # rviz_node,
+            foxglove_bridge,
+            control_node,
+            robot_state_publisher_node,
+            # robot_state_pub_node,
+            joint_state_broadcaster_spawner,
+            # robot_controller_spawner
+            delay_rviz_after_joint_state_broadcaster_spawner,
+            delay_robot_controller_spawner_after_joint_state_broadcaster_spawner,
+
+        ]
+    )
+    # nodes = [
+    #     control_node,
+    #     robot_state_pub_node,
+    #     joint_state_broadcaster_spawner,
+    #     delay_rviz_after_joint_state_broadcaster_spawner,
+    #     delay_robot_controller_spawner_after_joint_state_broadcaster_spawner,
+    # ]
