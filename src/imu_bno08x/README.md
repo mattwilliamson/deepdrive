6640dbbbfec01937f0ef7e44e507b5e8dfac1d0b
Matt Williamson <matt@aimatt.com>
2024-02-22 10:51:11 -0500

    IMU improvements

--- src/imu_bno08x/README.md
+++ src/imu_bno08x/README.md
@@ -3,10 +3,11 @@ imu_bno08x
 colcon build --symlink-install --packages-select imu_bno08x
 source /ros_ws/install/setup.sh
 ros2 run imu_bno08x imu_bno08x_publisher
+ros2 launch imu_bno08x imu_bno08x.launch.py
 ```
 
+TODO: shake detector only lasts for a moment. Set a trigger. Param?
 TODO: tf publisher to show imu working in rviz
-TODO: params
-TODO: covariance
+TODO: covariance?
 TODO: shake detection
 TODO: change IMU axes ENU etc
--- src/imu_bno08x/imu_bno08x/publisher.py
+++ src/imu_bno08x/imu_bno08x/publisher.py
@@ -2,7 +2,7 @@
 
 from sensor_msgs.msg import MagneticField, Imu
 from std_msgs.msg import Float64, Bool
-from diagnostic_msgs.msg import DiagnosticStatus
+from diagnostic_msgs.msg import DiagnosticStatus, KeyValue, DiagnosticArray
 from rclpy.node import Node
 import rclpy
 import busio
@@ -23,165 +23,276 @@ from adafruit_bno08x import (
 
 from adafruit_bno08x.i2c import BNO08X_I2C
 
-# TODO: Params for I2C bus and address and frame id and loop hz
-LOOP_HZ = 20
-DIAGNOSTIC_HZ = 1
+# TODO: Params for I2C bus
 
-# BNO080 (0x4b) BNO085 (0x4a)
-I2C_ADDRESS = 0x4B
-
+DIAGNOSTIC_HZ = 4
 IMU_TOPIC = "imu_bno08x/data"
-# MAG_TOPIC = 'imu_bno08x/mag'
+MAG_TOPIC = 'imu_bno08x/mag'
 DIAGNOSTIC_TOPIC = "imu_bno08x/status"
-# SHAKE_TOPIC = "imu_bno08x/shake"
-# ACTIVITY_TOPIC = "imu_bno08x/activity"
-# STABILITY_TOPIC = "imu_bno08x/stability"
-IMU_FRAME = "imu_link"
-
-SAVE_CALIBRATION = False
+SHAKE_TOPIC = "imu_bno08x/shake"
+ACTIVITY_TOPIC = "imu_bno08x/activity"
+STABILITY_TOPIC = "imu_bno08x/stability"
 
 
 class ImuNode(Node):
+    """
+    This class represents a ROS node for publishing IMU data from the BNO08X sensor.
+
+    I2C Address: BNO080 (0x4b / 75) BNO085 (0x4a / 74)
+    
+    Parameters:
+    - save_calibration (bool): Flag indicating whether to save the calibration data. Default is False.
+    - publish_mag (bool): Flag indicating whether to publish magnetic field data. Default is False.
+    - imu_frame (str): The frame ID for the IMU data. Default is "imu_link".
+    - rate (int): The publishing rate in Hz. Default is 30.
+    - address (int): The I2C address of the BNO08X sensor. Default is 0x4B.
+    - frequency (int): The frequency of the I2C bus. Default is 800000.
+    
+    """
+
     def __init__(self):
         super().__init__("imu_bno08x_node")
         self.get_logger().info("node is starting up...")
 
+        self.save_calibration = self.declare_parameter('save_calibration', False).value
+        self.publish_mag = self.declare_parameter('publish_mag', False).value
+        self.imu_frame = self.declare_parameter('imu_frame', "imu_link").value
+        self.rate = self.declare_parameter('rate', 30).value
+        self.address = self.declare_parameter('address', 0x4B).value
+        self.frequency = self.declare_parameter('frequency', 800000).value
+
         self.calibration_status = 0
         self.saved_calibration = False
-        # self.shake_detected = False
-
-        # self.declare_parameter('my_parameter', 'world')
-        # my_param = self.get_parameter('my_parameter').get_parameter_value().string_value
 
         # i2c = I2C(1)
         # self.bno = BNO08X_I2C(i2c, address=0x4a)
         # This works for the Jetson Orin Nano
-        # i2c = busio.I2C(board.SCL, board.SDA)
-        i2c = busio.I2C(board.SCL, board.SDA, frequency=800000)
-        self.bno = BNO08X_I2C(i2c, address=I2C_ADDRESS)
+        i2c = busio.I2C(board.SCL, board.SDA)
+        # RuntimeWarning: I2C frequency is not settable in python, ignoring!
+        # i2c = busio.I2C(board.SCL, board.SDA, frequency=self.frequency)
+        self.bno = BNO08X_I2C(i2c, address=self.address)
 
         self.bno.enable_feature(BNO_REPORT_ROTATION_VECTOR)
         self.bno.enable_feature(BNO_REPORT_LINEAR_ACCELERATION)
         self.bno.enable_feature(BNO_REPORT_GYROSCOPE)
         self.bno.enable_feature(BNO_REPORT_MAGNETOMETER)
         self.bno.enable_feature(BNO_REPORT_ACCELEROMETER)
-        # self.bno.enable_feature(BNO_REPORT_SHAKE_DETECTOR)
-        # self.bno.enable_feature(BNO_REPORT_STABILITY_CLASSIFIER)
+        self.bno.enable_feature(BNO_REPORT_SHAKE_DETECTOR)
+        self.bno.enable_feature(BNO_REPORT_STABILITY_CLASSIFIER)
+        self.bno.enable_feature(BNO_REPORT_ACTIVITY_CLASSIFIER)
 
         self.imu_publisher = self.create_publisher(Imu, IMU_TOPIC, 10)
-        self.create_timer(1.0 / LOOP_HZ, self.publish_imu)
+        self.create_timer(1.0 / self.rate, self.publish_imu)
 
-        self.diagnostics = self.create_publisher(DiagnosticStatus, DIAGNOSTIC_TOPIC, 10)
+        self.diagnostics = self.create_publisher(DiagnosticArray, DIAGNOSTIC_TOPIC, 10)
         self.create_timer(1.0 / DIAGNOSTIC_HZ, self.publish_diagnostics)
 
-        # self.shake_publisher = self.create_publisher(Bool, SHAKE_TOPIC, 10)
-
-        # self.publisher = self.create_publisher(MagneticField, 'imu_bno08x/mag', 10)
+        if self.publish_mag:
+            self.mag_publisher = self.create_publisher(MagneticField, MAG_TOPIC, 10)
 
         time.sleep(0.5)  # ensure IMU is initialized
 
         self.get_logger().info("node is started")
 
     def publish_imu(self):
-        self.calibration_status = self.bno.calibration_status
-        self.calibration_status = 1
+            """
+            Publishes the IMU data as ROS messages.
 
-        msg = Imu()
-        msg.header.frame_id = IMU_FRAME
-        msg.header.stamp = self.get_clock().now().to_msg()
+            This function retrieves the IMU data from the BNO08x sensor and publishes it as ROS messages.
+            The IMU data includes orientation, linear acceleration, and angular velocity.
+            """
+            
+            self.calibration_status = self.bno.calibration_status
+            self.calibration_status = 1
 
-        # Increase the covariance for the orientation if the calibration status is low
-        # Status: 0=Unreliable 1=Accuracy Low 2=Accuracy Medium 3=Accuracy High 
-        cov = int(5 - self.calibration_status)
-        cov = (cov * cov * .02) - .079 # Weigh lower accuracy even less
-        # 0 unreliable -> 0.421
-        # 1 low        -> 0.241
-        # 2 medium     -> 0.100
-        # 3 high       -> 0.001
+            msg = Imu()
+            msg.header.frame_id = self.imu_frame
+            msg.header.stamp = self.get_clock().now().to_msg()
 
-        # Orientation
-        quat_i, quat_j, quat_k, quat_real = self.bno.quaternion
-        msg.orientation.w = quat_i
-        msg.orientation.x = quat_j
-        msg.orientation.y = quat_k
-        msg.orientation.z = quat_real
-        msg.orientation_covariance[0] = cov
-        msg.orientation_covariance[4] = cov
-        msg.orientation_covariance[8] = cov
+            # Increase the covariance for the orientation if the calibration status is low
+            # Status: 0=Unreliable 1=Accuracy Low 2=Accuracy Medium 3=Accuracy High 
+            cov = int(5 - self.calibration_status)
+            cov = (cov * cov * .02) - .079 # Weigh lower accuracy even less
+            # 0 unreliable -> 0.421
+            # 1 low        -> 0.241
+            # 2 medium     -> 0.100
+            # 3 high       -> 0.001
 
-        # Linear Acceleration with gravity removed
-        accel_x, accel_y, accel_z = self.bno.linear_acceleration
-        msg.linear_acceleration.x = accel_x
-        msg.linear_acceleration.y = accel_y
-        msg.linear_acceleration.z = accel_z
-        msg.linear_acceleration_covariance[0] = cov
-        msg.linear_acceleration_covariance[4] = cov
-        msg.linear_acceleration_covariance[8] = cov
+            # Orientation
+            quat_i, quat_j, quat_k, quat_real = self.bno.quaternion
+            msg.orientation.w = quat_i
+            msg.orientation.x = quat_j
+            msg.orientation.y = quat_k
+            msg.orientation.z = quat_real
+            msg.orientation_covariance[0] = cov
+            msg.orientation_covariance[4] = cov
+            msg.orientation_covariance[8] = cov
 
-        # Gyro
-        gyro_x, gyro_y, gyro_z = self.bno.gyro
-        msg.angular_velocity.x = gyro_x
-        msg.angular_velocity.y = gyro_y
-        msg.angular_velocity.z = gyro_z
-        msg.angular_velocity_covariance[0] = cov
-        msg.angular_velocity_covariance[4] = cov
-        msg.angular_velocity_covariance[8] = cov
+            # Linear Acceleration with gravity removed
+            accel_x, accel_y, accel_z = self.bno.linear_acceleration
+            msg.linear_acceleration.x = accel_x
+            msg.linear_acceleration.y = accel_y
+            msg.linear_acceleration.z = accel_z
+            msg.linear_acceleration_covariance[0] = cov
+            msg.linear_acceleration_covariance[4] = cov
+            msg.linear_acceleration_covariance[8] = cov
 
-        # Accelerometer
-        # accel_x, accel_y, accel_z = self.bno.acceleration
-        # msg.linear_acceleration.x = accel_x
-        # msg.linear_acceleration.y = accel_y
-        # msg.linear_acceleration.z = accel_z
-        # msg.linear_acceleration_covariance[0] = cov
-        # msg.linear_acceleration_covariance[4] = cov
-        # msg.linear_acceleration_covariance[8] = cov
+            # Gyro
+            gyro_x, gyro_y, gyro_z = self.bno.gyro
+            msg.angular_velocity.x = gyro_x
+            msg.angular_velocity.y = gyro_y
+            msg.angular_velocity.z = gyro_z
+            msg.angular_velocity_covariance[0] = cov
+            msg.angular_velocity_covariance[4] = cov
+            msg.angular_velocity_covariance[8] = cov
 
-        self.imu_publisher.publish(msg)
+            self.imu_publisher.publish(msg)
 
-        # Magnetometer
-        # mag_x, mag_y, mag_z = self.bno.magnetic
-        # mag_msg.header.stamp = rospy.Time.now()
-        # mag_msg.magnetic_field.x = mag_x
-        # mag_msg.magnetic_field.y = mag_y
-        # mag_msg.magnetic_field.z = mag_z
-        # mag_msg.magnetic_field_covariance[0] = cov
-        # mag_msg.magnetic_field_covariance[4] = cov
-        # mag_msg.magnetic_field_covariance[8] = cov
+            # Magnetometer
+            if self.publish_mag:
+                mag_msg = MagneticField()
+                mag_msg.header.frame_id = self.imu_frame
+                mag_msg.header.stamp = self.get_clock().now().to_msg()
+                mag_x, mag_y, mag_z = self.bno.magnetic
+                mag_msg.magnetic_field.x = mag_x
+                mag_msg.magnetic_field.y = mag_y
+                mag_msg.magnetic_field.z = mag_z
+                mag_msg.magnetic_field_covariance[0] = cov
+                mag_msg.magnetic_field_covariance[4] = cov
+                mag_msg.magnetic_field_covariance[8] = cov
+                self.mag_publisher(mag_msg)
 
-        # shake_detected = self.bno.shake
-        # if shake_detected:
-        #     self.get_logger().info("Shake detected")
-        # if self.shake_detected != shake_detected and type(shake_detected) is bool:
-        #     self.shake_detected = shake_detected
-        #     self.shake_publisher.publish(Bool(data=shake_detected))
-
-
-
-
-    def publish_diagnostics(self):
+    def get_diagnostic_status(self):
         status_msg = DiagnosticStatus()
-        # OK=0, WARN=1, ERROR=2, STALE=3
-        status_msg.level = b"\x00"
-        status_msg.name = "bno08x IMU"
+        status_msg.name = "IMU Calibration"
+        status_msg.hardware_id = "bno08x"
         status_msg.message = REPORT_ACCURACY_STATUS[self.calibration_status]
-
+        status_msg.values = [
+            KeyValue(key="Calibration Code", value=str(self.calibration_status)),
+            KeyValue(key="Calibration Status", value=REPORT_ACCURACY_STATUS[self.calibration_status]),
+        ]
+        
         # Status: 0=Unreliable 1=Accuracy Low 2=Accuracy Medium 3=Accuracy High 
         if self.calibration_status == 3:
-            status_msg.level = b"\x00"
+            # Accuracy High 
+            status_msg.level = DiagnosticStatus.OK
             if not self.saved_calibration:
                 # Calibration status is good. Let's persist it.
                 self.get_logger().info("accuracy calibration is good. Saving.")
-                if SAVE_CALIBRATION:
+                if self.save_calibration:
                     self.bno.save_calibration_data()
                 self.saved_calibration = True
-        if self.calibration_status == 2:
-            status_msg.level = b"\x00"
+        elif self.calibration_status == 2:
+            # Accuracy Medium
+            status_msg.level = DiagnosticStatus.OK
+        elif self.calibration_status == 1:
+            # Accuracy LOW
+            status_msg.level = DiagnosticStatus.WARN
         else:
-            # Warning
-            status_msg.level = b"\x01"
+            # Unreliable
+            status_msg.level = DiagnosticStatus.ERROR
 
-        self.diagnostics.publish(status_msg)
+        return status_msg
+    
+    def get_shake_status(self):
+        shake_detected = self.bno.shake
 
+        status_msg = DiagnosticStatus()
+        status_msg.level = DiagnosticStatus.OK
+        status_msg.name = "Shake Detector"
+        status_msg.hardware_id = "bno08x"
+        status_msg.level = DiagnosticStatus.WARN if shake_detected else DiagnosticStatus.OK
+        status_msg.message = "Shake Detected" if shake_detected else "No Shake Detected"
+        status_msg.values = [
+            KeyValue(key="detected", value=str(shake_detected)),
+        ]
+
+        return status_msg
+    
+    def get_stability_status(self):
+            """
+            Get the stability status of the BNO08x IMU. One of:
+                * "Unknown" - The sensor is unable to classify the current stability
+                * "On Table" - The sensor is at rest on a stable surface with very little vibration
+                * "Stationary" -  The sensor’s motion is below the stable threshold but\
+                the stable duration requirement has not been met. This output is only available when\
+                gyro calibration is enabled
+                * "Stable" - The sensor’s motion has met the stable threshold and duration requirements.
+                * "In motion" - The sensor is moving.
+
+            Returns:
+                DiagnosticStatus: The stability status message.
+            """
+            
+            stability = self.bno.stability_classification
+
+            status_msg = DiagnosticStatus()
+            status_msg.level = DiagnosticStatus.OK
+            status_msg.name = "Stability Classifier"
+            status_msg.hardware_id = "bno08x"
+            status_msg.level = DiagnosticStatus.OK if stability in ("Stable", "On Table") else DiagnosticStatus.WARN
+            status_msg.message = stability
+            status_msg.values = [
+                KeyValue(key="stability", value=str(stability)),
+            ]
+
+            return status_msg
+    
+    def get_activity_status(self):
+        """
+        Get the activity status of the BNO08x IMU. One of:
+            * "Unknown"
+            * "In-Vehicle"
+            * "On-Bicycle"
+            * "On-Foot"
+            * "Still"
+            * "Tilting"
+            * "Walking"
+            * "Running"
+            * "On Stairs"
+
+        {'most_likely': 'Unknown', 'Unknown': 91, 'In-Vehicle': 2, 'On-Bicycle': 0, 'On-Foot': 3, 'Still': 4, 'Tilting': 0, 'Walking': 1, 'Running': 2, 'OnStairs': 0}
+
+        Returns:
+            DiagnosticStatus: The activity status message.
+        """
+        activity = self.bno.activity_classification
+        most_likely = activity['most_likely']
+        status_msg = DiagnosticStatus()
+        status_msg.level = DiagnosticStatus.OK
+        status_msg.name = "Activity Classifier"
+        status_msg.hardware_id = "bno08x"
+        status_msg.message = most_likely
+        status_msg.level = DiagnosticStatus.OK 
+        if most_likely in ("Unknown", "Tilting"):
+            status_msg.level = DiagnosticStatus.WARN
+        elif most_likely in ("On Stairs", "Running", "On-Foot"):
+            status_msg.level = DiagnosticStatus.ERROR
+
+        status_msg.values = [KeyValue(key=k, value=str(v)) for k, v in activity.items()]
+        return status_msg
+
+
+    def publish_diagnostics(self):
+            """
+            Publishes diagnostic information about the bno08x IMU.
+
+            This method creates a DiagnosticStatus message and populates it with relevant information
+            such as the calibration status of the IMU. The message is then published using the
+            diagnostics publisher.
+
+            :return: None
+            """
+            
+            status_array = DiagnosticArray()
+            status_array.status = [
+                self.get_diagnostic_status(),
+                self.get_shake_status(),
+                self.get_stability_status(),
+                self.get_activity_status(),
+            ]
+            status_array.header.stamp = self.get_clock().now().to_msg()
+            self.diagnostics.publish(status_array)
 
 
 def main(args=None):
@@ -189,8 +300,7 @@ def main(args=None):
     try:
         node = ImuNode()
         rclpy.spin(node)
-        node.destroy_node()
     except KeyboardInterrupt:
         pass
 
-    # rclpy.shutdown()
+    rclpy.shutdown()
