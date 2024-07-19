097f62151126bad56ddbc420a70673f686a36fb2
Matt Williamson <matt@aimatt.com>
2024-07-19 10:56:46 -0400

    fixed lidar

--- src/deepdrive_lidar/launch/ld19_lidar.launch.py
+++ src/deepdrive_lidar/launch/ld19_lidar.launch.py
@@ -9,7 +9,7 @@ def generate_launch_description():
         [
             Node(
                 package="ld19_lidar",
-                executable="ld19_lidar_node",
+                executable="ld19_node",
                 name="ld19_lidar_node",
                 output="screen",
                 parameters=[
